9bd1b9308dfa6ba635fe6132e873cb7fb1da3c56
Vidhya Sathyalaya Ramakrishnan <vsathyal@asu.edu>
2022-02-18 23:41:22 -0700

    Fixing the amplify ui react library error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Router from './Router.js'
-import { withAuthenticator } from 'aws-amplify-react'
-
+import Amplify, { Auth } from "aws-amplify";
+import awsconfig from "./aws-exports";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+Amplify.configure(awsconfig);
+Auth.configure(awsconfig);
 function App() {
   return (
     <div className="App">
