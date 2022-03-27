d723f463cc61170d91da46dab0cbae8ba7384880
aprabha7 <77811284+aprabha7@users.noreply.github.com>
2022-03-27 15:45:11 -0700

    US_106_NavBar_routing
    Updated the nav bar font page and merged with the routing page

--- src/Home.js
+++ src/Home.js
@@ -10,39 +10,42 @@ import awsconfig from './aws-exports';
 import { NavBar } from './ui-components';
 import { FrontPage1 } from './ui-components';
 import { Footer1 } from './ui-components';
+import { 
+  Frame418 
+} from './ui-components';
 Amplify.configure(awsconfig);
 
 
 function Home() {
-
   const imageOverrides = {
-   "image":{
-      src: "https://www.bootdey.com/app/webroot/img/Content/avatar/avatar1.png",
-   },
-  }
-  const fpg1 = {
-    "image 1":{
-      src: "https://i.ibb.co/xfyV5Dd/Screen-Shot-2022-03-18-at-1-15-07-PM.png",
+    "image":{
+       src: "https://www.bootdey.com/app/webroot/img/Content/avatar/avatar1.png",
     },
-  }
-
-  const fott1 ={
-    "image 1":{
-      src: "https://static.vecteezy.com/system/resources/previews/002/448/934/non_2x/couple-chatting-in-the-smartphone-screen-virtual-relationship-dating-app-vector.jpg"
-    },
-  }
-  return (
-    // <AmplifyProvider>
-        <div className='App'>
-          <p align="center"> Relationship Journal</p>
-          <NavBar overrides={imageOverrides} />
-       
-        <div>
-          <FrontPage1 overrides={fpg1}/>
-          <Footer1 overrides={fott1}/>
-        </div>
-        </div>
-      // </AmplifyProvider>
+   }
+   const fpg1 = {
+     "image 1":{
+       src: "https://i.ibb.co/xfyV5Dd/Screen-Shot-2022-03-18-at-1-15-07-PM.png",
+     },
+   }
+ 
+   const fott1 ={
+     "image 1":{
+       src: "https://static.vecteezy.com/system/resources/previews/002/448/934/non_2x/couple-chatting-in-the-smartphone-screen-virtual-relationship-dating-app-vector.jpg"
+     },
+   }
+   return (
+     // <AmplifyProvider>
+         <div className='App'>
+           <p align="center"> Relationship Journal</p>
+           <NavBar overrides={imageOverrides} />
+        
+         <div>
+           <FrontPage1 overrides={fpg1}/>
+           <Footer1 overrides={fott1}/>
+           <Frame418 />
+         </div>
+         </div>
+       // </AmplifyProvider>
   );
 }
 
