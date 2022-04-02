496a9c95accf1a14bc84b8ceedd7f7f88b2caa3c
Vidhya Sathyalaya Ramakrishnan <vsathyal@asu.edu>
2022-04-01 18:31:48 -0700

    US107T135 Journal backend create

--- src/App.js
+++ src/App.js
@@ -4,12 +4,32 @@ import Router from './Router.js'
 import Amplify, { Auth } from "aws-amplify";
 import awsconfig from "./aws-exports";
 import { withAuthenticator } from "@aws-amplify/ui-react";
+import CreateJournalsamplebutton from './ui-components/CreateJournalsamplebutton';
+import { Journal } from './models';
+import { DataStore } from '@aws-amplify/datastore';
+
+import AWSDateUtil from './util'
 Amplify.configure(awsconfig);
 Auth.configure(awsconfig);
+
+
+async function saveJournal(){
+  const journalrow = await DataStore.save(
+            new Journal({
+            "date_created":  AWSDateUtil.getCurrentAWSDate(),
+            "is_active": true
+          })
+        );
+
+        console.log('Created Journal ID : ', journalrow.id)
+    }
+    
 function App() {
   return (
     <div className="App">
       <Router/>
+      <CreateJournalsamplebutton 
+        onClick ={ () => saveJournal()}/>
     </div>
   );
 }
