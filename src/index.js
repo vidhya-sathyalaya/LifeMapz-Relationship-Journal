406f350fccddc25954faf2615c63532a52543ffd
Arjunkumar Borkhatariya <81658573+arjunsb21@users.noreply.github.com>
2022-05-03 13:15:19 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,38 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { JournalEntryFeedCollection } from './ui-components';
+import reportWebVitals from './reportWebVitals';import './App.css';
 import Amplify from 'aws-amplify';
 import "@aws-amplify/ui-react/styles.css";
-import awsExports from "./aws-exports";
-import {AmplifyProvider} from "@aws-amplify/ui-react";
+import { AmplifyProvider, Authenticator } from "@aws-amplify/ui-react";
 import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
-
+import awsExports from "./aws-exports";
 
 Amplify.configure(awsExports);
-
+Amplify.configure(awsconfig);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AmplifyProvider>
+      <Authenticator.Provider>
+      <App />
+      </Authenticator.Provider>
+    </AmplifyProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//       </header>
-//       <p> Hello World! </p>
-//     </div>
-//   );
-//  }
-//  export default App;
- 
