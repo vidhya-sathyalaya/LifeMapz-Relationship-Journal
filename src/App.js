7ace7f9d7bd6fd058fbd10c08b1716b83b902ffd
Arjunkumar Borkhatariya <81658573+arjunsb21@users.noreply.github.com>
2022-05-03 13:16:54 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,20 @@
-import logo from './logo.svg';
-import { CardACollection } from './ui-components';
+import './App.css';
 import Amplify from 'aws-amplify';
 import "@aws-amplify/ui-react/styles.css";
-import {AmplifyProvider} from "@aws-amplify/ui-react";
+import RoutingFile from './RoutingFile';
 import awsconfig from './aws-exports';
-import './App.css';
-import { CardC } from './ui-components';
-import { JournalEntryFeedCollection } from './ui-components';
-import { Footer2 } from './ui-components';
-import { JournalEntryPageGrid } from './ui-components';
-import { 
-  ViewProfA 
-} from './ui-components';
+
+import { withAuthenticator } from '@aws-amplify/ui-react'
 Amplify.configure(awsconfig);
+
 
 function App() {
 
-  const buttonOveride = {
-    "imageauq":{
-      src: "https://d3i6fh83elv35t.cloudfront.net/static/2020/05/GettyImages-1196354779-1200x814.png"
-    },
-    "pngegg 2":{
-      src: "https://i.ibb.co/LPFnj5d/pngegg.png"
-    }
-  }
-  return (
-    <div className="App">
-        <AmplifyProvider>
-          <CardACollection />
-          <CardC overrides={buttonOveride}/>
-          <ViewProfA overrides={buttonOveride}/>
-          <Footer2 />
-        </AmplifyProvider>
-       
-    </div>
+    return (
+      <div>
+        <RoutingFile classname="RoutingFile"></RoutingFile>
+      </div>
   );
 }
-
-export default App;
+ export default App;
+// export default withAuthenticator(App);
